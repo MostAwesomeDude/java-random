87b050bdc18175035fe27de5fef131dbe6845a7c
Corbin Simpson <simpsoco@osuosl.org>
2010-11-05 17:09:59 -0700

    tests: Pad out rest of tests.

--- tests.py
+++ tests.py
@@ -22,9 +22,30 @@ class AccuracyTest(unittest.TestCase):
         standard = -1155484576
         self.assertEqual(self.r.nextInt(), standard)
 
+    def test_nextInt_lue(self):
+        standard = 12
+        self.assertEqual(self.r.nextInt(42), standard)
+
     def test_nextLong(self):
         standard = -4962768465676381896
         self.assertEqual(self.r.nextLong(), standard)
+
+    def test_nextBoolean(self):
+        standard = True
+        self.assertEqual(self.r.nextBoolean(), standard)
+
+    def test_nextFloat(self):
+        standard = 0.730968
+        self.assertEqual(self.r.nextFloat(), standard)
+
+    def test_nextDouble(self):
+        standard = 0.730968
+        self.assertEqual(self.r.nextDouble(), standard)
+
+    def test_nextGaussian(self):
+        standard = 0.802533, -0.901546
+        self.assertEqual((self.r.nextGaussian(), self.r.nextGaussian()),
+            standard)
 
 if __name__ == "__main__":
     unittest.main()
